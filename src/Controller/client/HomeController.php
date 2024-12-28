c6a5f4493cd7834fe760ae172c2957f583820725
AHIKPA <jeanchristianahikpa@gmail.com>
2024-12-28 05:17:57 +0100

    Page liste des produits et détails OK

--- src/Controller/client/HomeController.php
+++ src/Controller/client/HomeController.php
@@ -2,15 +2,40 @@
 
 namespace App\Controller\client;
 
+use App\Entity\Fruits;
+use App\Repository\FruitsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(FruitsRepository $fruitsRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        // Récupérer tous les fruits depuis la base de données
+        $fruits = $fruitsRepository->findAll();
+
+        // Passer les fruits à la vue
+
+        return $this->render('home/index.html.twig', [
+            'fruits' => $fruits]);
     }
+
+
+    #[Route('/fruit/{id}', name: 'app_fruit_details')]
+    public function show(Fruits $fruit): Response
+    {
+        // Vérifier si le fruit existe
+        if (!$fruit) {
+            throw $this->createNotFoundException('Fruit not found');
+        }
+
+        // Afficher la page de détails du fruit
+        return $this->render('home/show.html.twig', [
+            'fruit' => $fruit
+        ]);
+    }
+
 }
